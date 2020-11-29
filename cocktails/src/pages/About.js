07d39289898c2d982b50ed82f642a77f2a72f5bd
Baal89 <efestos89@gmail.com>
2020-11-29 17:10:16 +0000

    react cocktail list

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+        <h1 className="section-title">about us</h1>
+        <p>lorem ipsum dolor sit amet consectutur adipisicing elit
+            Eos doloribus animi pariatur fifnissimos nulla lure dolorum et fugit minus hic. 
+            Adipisci sint enim, sit quo impedit doloribus accusantissum, ipsum esse
+            distinctin accusamus laborum illum similique cupiditate cinsequatur quasi ab.lure.
+        </p>
+    </section>
+  )
+}
+
+export default About
